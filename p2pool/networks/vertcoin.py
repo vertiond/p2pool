e0071f06caf38dde79592b00313141483a78fa8b
Vertian <uniduckdavid@gmail.com>
2020-04-26 16:49:05 -0500

    new file:   p2pool/bitcoin/networks/vertcoin.py 	new file:   p2pool/bitcoin/networks/vertcoin_testnet.py 	new file:   p2pool/networks/vertcoin.py 	new file:   p2pool/networks/vertcoin2.py 	new file:   p2pool/networks/vertcoin_testnet.py

---
+++ p2pool/networks/vertcoin.py
@@ -0,0 +1,21 @@
+from p2pool.bitcoin import networks
+
+PARENT=networks.nets['vertcoin']
+SHARE_PERIOD=15 # seconds
+CHAIN_LENGTH=24*60*60//10 # shares
+REAL_CHAIN_LENGTH=24*60*60//10 # shares
+TARGET_LOOKBEHIND=200 # shares
+SPREAD=12 # blocks
+IDENTIFIER='a06a81c827cab983'.decode('hex')
+PREFIX='7c3614a6bcdcf784'.decode('hex')
+P2P_PORT=9346
+MIN_TARGET=4
+MAX_TARGET=2**256//2**20 - 1
+PERSIST=False
+WORKER_PORT=9171
+BOOTSTRAP_ADDRS='vtc.alwayshashing.com crypto.office-on-the.net pool.vtconline.org p2pool.kosmoplovci.org uk1.vtconline.org pool.boxienet.net'.split(' ')
+ANNOUNCE_CHANNEL='#p2pool-vtc'
+VERSION_CHECK=lambda v: True
+SOFTFORKS_REQUIRED = set(['nversionbips', 'csv', 'segwit'])
+SEGWIT_ACTIVATION_VERSION = 16
+MINIMUM_PROTOCOL_VERSION = 1800
